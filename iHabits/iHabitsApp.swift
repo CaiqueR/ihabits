74372406d25c03d9a0996b866ce152a4232d65e2
Caique Ribeiro de Oliveira <caique@usekojo.com>
2023-06-09 18:55:17 -0300

    Initial Commit

---
+++ iHabits/iHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHabitsApp.swift
+//  iHabits
+//
+//  Created by Caique Ribeiro on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
